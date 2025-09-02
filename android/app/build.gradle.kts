8544982833b352f457f0dcd7b80fb431e5354c07
Gold-Lio <studiosaan33@gmail.com>
2025-09-02 23:15:08 +0900

    bundleRelease_Done

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -5,8 +8,14 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+val keystorePropertiesFile = rootProject.file("key.properties")
+val keystoreProperties = Properties()
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 android {
-    namespace = "com.example.dd"
+    namespace = "com.numerology.finder"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973" // Explicitly set NDK version to fix mismatch
 
@@ -21,9 +30,19 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
+    // ⭐️ Add this signingConfigs block.
+    signingConfigs {
+        create("release") {
+            storeFile = file(keystoreProperties.getProperty("storeFile"))
+            storePassword = keystoreProperties.getProperty("storePassword")
+            keyAlias = keystoreProperties.getProperty("keyAlias")
+            keyPassword = keystoreProperties.getProperty("keyPassword")
+        }
+    }
+
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.dd"
+        applicationId = "com.numerology.finder"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -36,7 +55,12 @@ android {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // ⭐️ This is where we tell Gradle to use our release signing config.
+            if (keystorePropertiesFile.exists()) {
+                signingConfig = signingConfigs.getByName("release")
+            } else {
+                signingConfig = signingConfigs.getByName("debug")
+            }
         }
     }
 }
--- android/app/src/main/kotlin/com/numerology/finder/MainActivity.kt
+++ android/app/src/main/kotlin/com/numerology/finder/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.dd
+package com.numerology.finder
 
 import io.flutter.embedding.android.FlutterActivity
 
